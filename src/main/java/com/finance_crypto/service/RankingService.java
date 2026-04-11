215ff8d46bd89d167b7aa5e700eb485b8646c462
Igor Olivio <igorolivio123@gmail.com>
2026-04-10 23:27:06 -0300

    feat: add method to list assets with loss

--- src/main/java/com/finance_crypto/service/RankingService.java
+++ src/main/java/com/finance_crypto/service/RankingService.java
@@ -2,6 +2,7 @@ package com.finance_crypto.service;
 
 import com.finance_crypto.dto.RankingAtivoDTO;
 import org.springframework.stereotype.Service;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,19 @@ import java.util.List;
 public class RankingService {
 
     public List<RankingAtivoDTO> obterRankingCalculado() {
-        //Proximo passo: Implementar a lógica real de cálculo do ranking com base nos dados do banco de dados.
+        // Proximo passo: Implementar a lógica real de cálculo do ranking com base nos dados do banco de dados.
+
         // Dados mockados para teste
         return Arrays.asList(
-            new RankingAtivoDTO("BTC", 300000.0, 350000.0, 16.6),
-            new RankingAtivoDTO("ETH", 15000.0, 18000.0, 20.0),
-            new RankingAtivoDTO("SOL", 500.0, 450.0, -10.0)
+                new RankingAtivoDTO("BTC", 300000.0, 350000.0, 16.6),
+                new RankingAtivoDTO("ETH", 15000.0, 18000.0, 20.0),
+                new RankingAtivoDTO("SOL", 500.0, 450.0, -10.0)
         );
+    }
+
+    public List<RankingAtivoDTO> obterAtivosComPrejuizo() {
+        return obterRankingCalculado().stream()
+                .filter(ativo -> ativo.getPercentualLucro() < 0)
+                .toList();
     }
 }
