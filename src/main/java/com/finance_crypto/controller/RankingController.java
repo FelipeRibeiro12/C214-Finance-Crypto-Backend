c61d98a70a14a4cbe0745b8255e1b838706bd68b
Igor Olivio <igorolivio123@gmail.com>
2026-04-10 23:29:30 -0300

    feat: add ranking loss endpoint

--- src/main/java/com/finance_crypto/controller/RankingController.java
+++ src/main/java/com/finance_crypto/controller/RankingController.java
@@ -2,24 +2,25 @@ package com.finance_crypto.controller;
 
 import com.finance_crypto.dto.RankingAtivoDTO;
 import com.finance_crypto.service.RankingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/ranking")
 public class RankingController {
 
-    private final RankingService rankingService;
-
-    // O Spring injeta o Service automaticamente aqui
-    public RankingController(RankingService rankingService) {
-        this.rankingService = rankingService;
-    }
+    @Autowired
+    private RankingService rankingService;
 
     @GetMapping("/lucrativos")
     public List<RankingAtivoDTO> obterRanking() {
         return rankingService.obterRankingCalculado();
+    }
+
+    @GetMapping("/prejuizos")
+    public List<RankingAtivoDTO> obterAtivosComPrejuizo() {
+        return rankingService.obterAtivosComPrejuizo();
     }
 }
